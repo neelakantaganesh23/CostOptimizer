830bb3768aeade51427db5ac9770295645961bbe
Ganesh Neelakanta <130562632+neelakantaganesh23@users.noreply.github.com>
2024-06-28 16:04:46 +0530

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -46,7 +46,7 @@ def get_custom_css():
 custom_css = get_custom_css()
 st.markdown(custom_css, unsafe_allow_html=True)
 st.sidebar.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-#snowflake_config = st.secrets["sf_usage_app"]
+snowflake_config = st.secrets["sf_usage_app"]
 #connect to snowflake function
 
 
